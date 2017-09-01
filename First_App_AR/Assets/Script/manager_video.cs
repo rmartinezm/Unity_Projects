a32d3386bae3ed07fa0a9aa17cf50ef779fbefb0
rmartinezm <robertomtz@ciencias.unam.mx>
2017-09-01 12:19:15 -0500

    Proyectos con implementacion de Realidad Aumentada

---
+++ First_App_AR/Assets/Script/manager_video.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class manager_video : MonoBehaviour {
+
+    public GameObject planeCat;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    public VideoClip videoToPlay;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void StartVideo()
+    {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+    }
+
+    public IEnumerator playVideo()
+    {
+        videoPlayer = planeCat.AddComponent<VideoPlayer>();
+        audioSource = planeCat.AddComponent<AudioSource>();
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        audioSource.Pause();
+
+        videoPlayer.source = VideoSource.VideoClip;
+
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        videoPlayer.clip = videoToPlay;
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        while (!videoPlayer.isPrepared)
+        {
+            yield return null;
+        }
+
+        //Play Video
+        videoPlayer.Play();
+
+        //Play Sound
+        audioSource.Play();
+    }
+}
---
+++ First_App_AR/Assets/Script/planeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool visible = GetComponent<Renderer>().isVisible;
+        if (visible)
+        {
+            gameObject.SendMessage("StartVideo");
+
+        }
+    }
+    
+}
